c2a538ced13ffadf7a20a9b9ec085568248d1281
Mbob023 <armcgrathmedia@gmail.com>
2016-04-21 11:21:30 -0500

    post for newUser

--- server/modules/index.js
+++ server/modules/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
-var db_query = require("../models/db_query.js");
+var User = require("../models/user_schema.js");
 
 router.get("/getnames", function(req, res){
   db_query.find({}, function(err, data){
@@ -11,6 +11,26 @@ router.get("/getnames", function(req, res){
     res.send(data);
   });
 });
+
+
+// POST for creating new user in db
+router.post('/newuser', function (req, res){
+  var request = req.body
+  var newUser = new User({ 'firstname' : request.firstname, 'lastname' : request.lastname,
+  'email' : request.email, 'username' : request.username, 'password' : request.password,
+  'role' : request.role, 'default_view' : request.default_view });
+  newUser.save(function(err, data) {
+
+    if (err) {
+      console.log('Error Saving newUser to Database', err);
+
+    }
+
+    res.send(data);
+    
+  });
+});
+
 
 router.post("/postnames", function (req, res) {
   var newName = new db_query({"name" : req.body.name});
