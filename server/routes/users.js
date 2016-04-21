d0329d5742f41ceb53b6c5d6eaeccfee726c5b73
Mark Rothermal <rothermal2@comcast.net>
2016-04-21 13:52:02 -0500

    added semi-colon.

--- server/routes/users.js
+++ server/routes/users.js
@@ -15,7 +15,7 @@ router.get("/getnames", function(req, res){
 });
 
 router.post("/postnames", function (req, res) {
-  var request = req.body
+  var request = req.body;
     var newUser = new User({ 'firstname' : request.firstname, 'lastname' : request.lastname,
     'email' : request.email, 'username' : request.username, 'password' : request.password,
     'role' : request.role, 'default_view' : request.default_view });
