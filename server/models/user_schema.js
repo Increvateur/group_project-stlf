846f5de656d90b1971c75cd543c397956e55754e
Mbob023 <armcgrathmedia@gmail.com>
2016-04-21 11:09:49 -0500

    user schema version 1

---
+++ server/models/user_schema.js
@@ -0,0 +1,17 @@
+// USER SCHEMA FOR DB - Collection "USERS"
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// USER Schema
+var User = new Schema ({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  default_view: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Users', User);
