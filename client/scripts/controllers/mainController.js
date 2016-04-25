292a121934cc41de6053fd32ed476cec06e9070e
Mark Rothermal <rothermal2@comcast.net>
2016-04-25 11:27:14 -0500

    added mock data for testing front end

--- client/scripts/controllers/mainController.js
+++ client/scripts/controllers/mainController.js
@@ -1,23 +1,23 @@
 myApp.controller("MainController", ["$scope", "STLFModule", function($scope, STLFModule) {
 
-    $scope.names=[];
-
-  $scope.addName = function(data) {
-      var postItem = {
-      "firstname": data.firstname
-      };
-
-      STLFModule.postNames(postItem)
-      .then(function(){
-        $scope.names = STLFModule.data.response;
-      });
-
-    };
-
-  STLFModule.getNames()
-    .then(function(){
-      $scope.names = STLFModule.data.response;
-      console.log("Names Retrieved!", $scope.names);
-    });
+  //  $scope.names=[];
+  //
+  //$scope.addName = function(data) {
+  //    var postItem = {
+  //    "firstname": data.firstname
+  //    };
+  //
+  //    STLFModule.postNames(postItem)
+  //    .then(function(){
+  //      $scope.names = STLFModule.data.response;
+  //    });
+  //
+  //  };
+  //
+  //STLFModule.getNames()
+  //  .then(function(){
+  //    $scope.names = STLFModule.data.response;
+  //    console.log("Names Retrieved!", $scope.names);
+  //  });
 
 }]);
