fb0b5c5c050253b172a2085d6832ec5546b18ffa
Mark Rothermal <rothermal2@comcast.net>
2016-04-26 14:48:37 -0500

    removing unused files, fixed merge conflict in package json

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,10 +57,7 @@ module.exports = function(grunt) {
                 expand: true,
                 cwd: 'client/views/',
                 src: [
-                    "failure.html",
                     "index.html",
-                    "register.html",
-                    "user.html",
                     "routes/*.html",
                     "partials/*.html",
                     "templates/*.html"
