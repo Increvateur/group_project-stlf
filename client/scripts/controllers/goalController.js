152fa3af7223c0d8eecc662613521fa3ffc0054b
Matthew Keymer <mastercyrax@hotmail.com>
2016-04-29 17:55:08 -0500

    Fixed merge issues

--- client/scripts/controllers/goalController.js
+++ client/scripts/controllers/goalController.js
@@ -5,10 +5,7 @@ myApp.controller('AddGoalsController', ['$scope', 'GoalService', function($scope
   console.log('HI, @CONTROLLER - AddGoalsController Works!');
 
   var goalService = GoalService;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9f87fade2263dce147f6a51bcbb413058bea6627
+
   $scope.goals = {};
   // $scope.months = ['september', 'october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august'];
   $scope.monthsObj = {
@@ -69,7 +66,6 @@ myApp.controller('AddGoalsController', ['$scope', 'GoalService', function($scope
     $scope.fiscalyear = year;
     return $scope.fiscalyear;
   };
-<<<<<<< HEAD
 
   // console.log('~ @goalController after addFyKey - $scope.goals: ', $scope.goals);
 
@@ -82,28 +78,6 @@ myApp.controller('AddGoalsController', ['$scope', 'GoalService', function($scope
     // console.log('## @goalController in saveGoals - $scope.key, data: ', $scope.key, data);
     // addGoals[$scope.key] = data;
     console.log('! @goalController in saveGoals - data: ', data);
-=======
-  $scope.months = ['september', 'october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august'];
-  $scope.addGoals = {};
-=======
->>>>>>> 9f87fade2263dce147f6a51bcbb413058bea6627
-
-  // console.log('~ @goalController after addFyKey - $scope.goals: ', $scope.goals);
-
-  $scope.saveGoals = function(data) {
-<<<<<<< HEAD
-    console.log('@goalController.js data (AKA: addGoals: ', data);
->>>>>>> 40980157c704ba7401ab7889fcc9203ad8642a76
-=======
-    var addGoals = {};
-    console.log('@goalController.js data (AKA: goals: ', data);
-    console.log('@goalController.js fiscalyear: ', $scope.fiscalyear);
-
-    // $scope.key = $scope.fiscalyear;
-    // console.log('## @goalController in saveGoals - $scope.key, data: ', $scope.key, data);
-    // addGoals[$scope.key] = data;
-    console.log('! @goalController in saveGoals - data: ', data);
->>>>>>> 9f87fade2263dce147f6a51bcbb413058bea6627
     goalService.newGoals(data);
   };
 
--- client/scripts/factories/goalService.js
+++ client/scripts/factories/goalService.js
@@ -1,20 +1,8 @@
 myApp.factory("GoalService", ["$http", function($http) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9f87fade2263dce147f6a51bcbb413058bea6627
-  var newGoals = function(goals) {
-    console.log('*@FACTORY goalService, newGoals(goals): ', goals);
-
-
-<<<<<<< HEAD
-=======
   var newGoals = function(addGoals) {
     console.log('*@FACTORY goalService, newGoals(addGoals): ', addGoals);
->>>>>>> 40980157c704ba7401ab7889fcc9203ad8642a76
-=======
->>>>>>> 9f87fade2263dce147f6a51bcbb413058bea6627
+
     // $http.post('goals', data).then(function(response){
     //   // some sort of feedback that user was added to DB
     // });
