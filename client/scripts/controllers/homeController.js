0b77ccc0c7807f46017f42491ee9e4bd7ed5f39d
Matthew Keymer <mastercyrax@hotmail.com>
2016-04-29 14:25:33 -0500

    Updated charts for new master pull

--- client/scripts/controllers/homeController.js
+++ client/scripts/controllers/homeController.js
@@ -1,10 +1,10 @@
 /**
  * Created by JFCS on 4/22/16.
  */
-myApp.controller("HomeController", ["$scope", '$filter',"MockService",
+myApp.controller("HomeController", ["$scope", "$filter", "MockService", "$uibModal",
     // testing data for front end practice.
 
-    function($scope,$filter,MockService) {
+    function($scope, $filter, MockService, $uibModal) {
 
         var mockService = MockService;
 
@@ -25,7 +25,17 @@ myApp.controller("HomeController", ["$scope", '$filter',"MockService",
         //mockService.getCampaigns();
         //mockService.getHouseholds();
 
+        $scope.open = function(size) {
 
+            var modalInstance = $uibModal.open({
+              animation: true,
+              templateUrl: './assets/views/templates/chartContent.html',
+              controller: 'ChartContentController',
+              size: size,
+              keyboard: true,
+      				backdrop: 'static'
+            });
+          };
 
 
 
@@ -65,5 +75,168 @@ myApp.controller("HomeController", ["$scope", '$filter',"MockService",
 
 
 
+
+}]);
+
+myApp.controller("ChartContentController", ["$scope", "$uibModalInstance", function($scope, $uibModalInstance) {
+
+	$scope.chartType = function(type) {
+		if (type == "piechart") {
+			$scope.load = $scope.piechart;
+		} else {
+			$scope.load = $scope.bargraph;
+		}
+	};
+
+  $scope.piechart = function(data) {
+
+    AmCharts.makeChart("chartdiv",
+			{
+				"type": "pie",
+				"angle": 20,
+				"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+				"depth3D": 20,
+				"titleField": "country",
+				"valueField": "litres",
+				"fontSize": 12,
+				"theme": "default",
+				"allLabels": [],
+				"balloon": {},
+				"titles": [],
+				"dataProvider": [
+					{
+						"country": "Events",
+						"litres": "356.9"
+					},
+					{
+						"country": "Ireland",
+						"litres": 131.1
+					},
+					{
+						"country": "Germany",
+						"litres": 115.8
+					},
+					{
+						"country": "Australia",
+						"litres": 109.9
+					},
+					{
+						"country": "Austria",
+						"litres": 108.3
+					},
+					{
+						"country": "UK",
+						"litres": 65
+					},
+					{
+						"country": "Belgium",
+						"litres": "20"
+					}
+				]
+			}
+		);
+  };
+
+	$scope.bargraph = function(data) {
+
+    AmCharts.makeChart("chartdiv",
+			{
+				"type": "serial",
+				"categoryField": "date",
+				"dataDateFormat": "YYYY-MM-DD",
+				"autoMarginOffset": 40,
+				"marginRight": 60,
+				"marginTop": 60,
+				"fontSize": 13,
+				"theme": "default",
+				"categoryAxis": {
+					"parseDates": true
+				},
+				"chartCursor": {
+					"enabled": true
+				},
+				"chartScrollbar": {
+					"enabled": true
+				},
+				"trendLines": [],
+				"graphs": [
+					{
+						"columnWidth": 0.44,
+						"cornerRadiusTop": 8,
+						"dashLength": 4,
+						"fillAlphas": 0.51,
+						"id": "AmGraph-1",
+						"lineAlpha": 0.44,
+						"title": "graph 1",
+						"type": "column",
+						"valueField": "column-1"
+					},
+					{
+						"bullet": "square",
+						"bulletBorderAlpha": 1,
+						"bulletBorderThickness": 1,
+						"bulletSize": 16,
+						"id": "AmGraph-2",
+						"lineThickness": 3,
+						"title": "graph 2",
+						"valueField": "column-2"
+					}
+				],
+				"guides": [],
+				"valueAxes": [
+					{
+						"id": "ValueAxis-1",
+						"title": ""
+					}
+				],
+				"allLabels": [],
+				"balloon": {},
+				"titles": [],
+				"dataProvider": [
+					{
+						"date": "2014-03-01",
+						"column-1": 8,
+						"column-2": 5
+					},
+					{
+						"date": "2014-03-02",
+						"column-1": 6,
+						"column-2": 7
+					},
+					{
+						"date": "2014-03-03",
+						"column-1": 2,
+						"column-2": 3
+					},
+					{
+						"date": "2014-03-04",
+						"column-1": 1,
+						"column-2": 3
+					},
+					{
+						"date": "2014-03-05",
+						"column-1": 2,
+						"column-2": 1
+					},
+					{
+						"date": "2014-03-06",
+						"column-1": 3,
+						"column-2": 2
+					},
+					{
+						"date": "2014-03-07",
+						"column-1": 6,
+						"column-2": 8
+					}
+				]
+			}
+		);
+	};
+
+$scope.load = $scope.piechart;
+
+$scope.close = function () {
+    $uibModalInstance.close();
+  };
 
 }]);
